819b250b837dcd92f61d16a1ff19742984cfa2d3
PLKhang <plkhang090403@gmail.com>
2024-07-06 08:18:21 +0700

    form create login for employee

---
+++ QLGDNganHang/frmRegister.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace QLGDNganHang
+{
+    partial class frmRegister
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegister));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbxUsername = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLoginName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblLoginNameError = new System.Windows.Forms.Label();
+            this.lblPasswordError = new System.Windows.Forms.Label();
+            this.lblConfirmPasswordError = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCMND = new System.Windows.Forms.TextBox();
+            this.btnShowPassword = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(544, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(142, 96);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "ID:";
+            // 
+            // cbxUsername
+            // 
+            this.cbxUsername.FormattingEnabled = true;
+            this.cbxUsername.Location = new System.Drawing.Point(221, 93);
+            this.cbxUsername.Name = "cbxUsername";
+            this.cbxUsername.Size = new System.Drawing.Size(281, 28);
+            this.cbxUsername.TabIndex = 1;
+            this.cbxUsername.SelectedIndexChanged += new System.EventHandler(this.cbxUsername_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Login name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(85, 352);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Password:";
+            // 
+            // txtLoginName
+            // 
+            this.txtLoginName.Location = new System.Drawing.Point(221, 288);
+            this.txtLoginName.Name = "txtLoginName";
+            this.txtLoginName.Size = new System.Drawing.Size(281, 26);
+            this.txtLoginName.TabIndex = 2;
+            this.txtLoginName.TextChanged += new System.EventHandler(this.txtLoginName_TextChanged);
+            this.txtLoginName.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(221, 349);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(281, 26);
+            this.txtPassword.TabIndex = 2;
+            this.txtPassword.UseSystemPasswordChar = true;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnConfirm.Location = new System.Drawing.Point(221, 458);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(113, 46);
+            this.btnConfirm.TabIndex = 3;
+            this.btnConfirm.Text = "CONFIRM";
+            this.btnConfirm.UseVisualStyleBackColor = false;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.Location = new System.Drawing.Point(389, 458);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(113, 46);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(221, 412);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(281, 26);
+            this.txtConfirmPassword.TabIndex = 2;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            this.txtConfirmPassword.TextChanged += new System.EventHandler(this.txtConfirmPassword_TextChanged);
+            this.txtConfirmPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(21, 415);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Confirm Password:";
+            // 
+            // lblLoginNameError
+            // 
+            this.lblLoginNameError.AutoSize = true;
+            this.lblLoginNameError.ForeColor = System.Drawing.Color.Red;
+            this.lblLoginNameError.Location = new System.Drawing.Point(525, 291);
+            this.lblLoginNameError.Name = "lblLoginNameError";
+            this.lblLoginNameError.Size = new System.Drawing.Size(119, 20);
+            this.lblLoginNameError.TabIndex = 0;
+            this.lblLoginNameError.Text = "Enter this area";
+            // 
+            // lblPasswordError
+            // 
+            this.lblPasswordError.AutoSize = true;
+            this.lblPasswordError.ForeColor = System.Drawing.Color.Red;
+            this.lblPasswordError.Location = new System.Drawing.Point(525, 352);
+            this.lblPasswordError.Name = "lblPasswordError";
+            this.lblPasswordError.Size = new System.Drawing.Size(119, 20);
+            this.lblPasswordError.TabIndex = 0;
+            this.lblPasswordError.Text = "Enter this area";
+            // 
+            // lblConfirmPasswordError
+            // 
+            this.lblConfirmPasswordError.AutoSize = true;
+            this.lblConfirmPasswordError.ForeColor = System.Drawing.Color.Red;
+            this.lblConfirmPasswordError.Location = new System.Drawing.Point(525, 415);
+            this.lblConfirmPasswordError.Name = "lblConfirmPasswordError";
+            this.lblConfirmPasswordError.Size = new System.Drawing.Size(119, 20);
+            this.lblConfirmPasswordError.TabIndex = 0;
+            this.lblConfirmPasswordError.Text = "Enter this area";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(125, 151);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 20);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Role:";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Enabled = false;
+            this.txtFullName.Location = new System.Drawing.Point(672, 148);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(297, 26);
+            this.txtFullName.TabIndex = 5;
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Enabled = false;
+            this.txtPhoneNumber.Location = new System.Drawing.Point(672, 203);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(297, 26);
+            this.txtPhoneNumber.TabIndex = 5;
+            // 
+            // txtRole
+            // 
+            this.txtRole.Enabled = false;
+            this.txtRole.Location = new System.Drawing.Point(221, 148);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(281, 26);
+            this.txtRole.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(601, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "CMND:";
+            // 
+            // txtCMND
+            // 
+            this.txtCMND.Enabled = false;
+            this.txtCMND.Location = new System.Drawing.Point(672, 93);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(297, 26);
+            this.txtCMND.TabIndex = 5;
+            // 
+            // btnShowPassword
+            // 
+            this.btnShowPassword.Appearance.BorderColor = System.Drawing.Color.Green;
+            this.btnShowPassword.Appearance.Options.UseBorderColor = true;
+            this.btnShowPassword.AppearancePressed.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnShowPassword.AppearancePressed.Options.UseBorderColor = true;
+            this.btnShowPassword.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnShowPassword.ImageOptions.Image")));
+            this.btnShowPassword.ImageOptions.ImageIndex = 0;
+            this.btnShowPassword.ImageOptions.Location = DevExpress.XtraEditors.ImageLocation.MiddleRight;
+            this.btnShowPassword.Location = new System.Drawing.Point(464, 349);
+            this.btnShowPassword.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnShowPassword.Name = "btnShowPassword";
+            this.btnShowPassword.Size = new System.Drawing.Size(38, 26);
+            this.btnShowPassword.TabIndex = 6;
+            this.btnShowPassword.Click += new System.EventHandler(this.btnShowPassword_Click);
+            // 
+            // frmRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1000, 562);
+            this.Controls.Add(this.btnShowPassword);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLoginName);
+            this.Controls.Add(this.cbxUsername);
+            this.Controls.Add(this.lblConfirmPasswordError);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblPasswordError);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblLoginNameError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmRegister";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmRegister_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbxUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLoginName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblLoginNameError;
+        private System.Windows.Forms.Label lblPasswordError;
+        private System.Windows.Forms.Label lblConfirmPasswordError;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCMND;
+        private DevExpress.XtraEditors.SimpleButton btnShowPassword;
+    }
+}
---
+++ QLGDNganHang/frmRegister.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLGDNganHang
+{
+    public partial class frmRegister : Form
+    {
+        private string loginName;
+        private string password;
+        private string username;
+        private DataTable dt = new DataTable();
+        private DataTable dtLoginName = new DataTable();
+        private bool isShowingPassword = false;
+        private bool checkLoginName = false, checkPassword = false, checkConfirmPassword = false;
+        public frmRegister()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void frmRegister_Load(object sender, EventArgs e)
+        {
+            dt = Program.ExecStoredProcedureReturnTable("SELECT * FROM V_EX_LoginName");
+            dtLoginName = Program.ExecStoredProcedureReturnTable("SELECT * FROM DBO.V_LOGINNAME");
+
+            cbxUsername.DataSource = dt;
+            cbxUsername.DisplayMember = "MANV";
+            cbxUsername.ValueMember = "MANV";
+            cbxUsername.SelectedIndex = 0;
+            cbxUsername.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            txtRole.Text = Program.mRole;
+
+            txtCMND.Text = dt.Rows[cbxUsername.SelectedIndex]["CMND"].ToString();
+            txtFullName.Text = dt.Rows[cbxUsername.SelectedIndex]["HOTEN"].ToString();
+            txtPhoneNumber.Text = dt.Rows[cbxUsername.SelectedIndex]["SODT"].ToString();
+        }
+        private bool IsExistedLoginName(string loginName)
+        {
+            foreach (DataRow row in dtLoginName.Rows)
+            {
+                if (loginName.Equals(row["LoginName"].ToString()))
+                { 
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private void txtLoginName_TextChanged(object sender, EventArgs e)
+        {
+            if (txtLoginName.Text.Length == 0)
+            {
+                lblLoginNameError.ForeColor = Color.Red;
+                lblLoginNameError.Text = "Enter this area!";
+                checkLoginName = false;
+            }
+            else if (IsExistedLoginName(txtLoginName.Text))
+            {
+                lblLoginNameError.ForeColor = Color.Red;
+                lblLoginNameError.Text = "This login name have been existed!";
+                checkLoginName = false;
+            }
+            else
+            {
+                lblLoginNameError.ForeColor = Color.Green;
+                lblLoginNameError.Text = "✅";
+                checkLoginName = true;
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Length == 0)
+            {
+                lblPasswordError.ForeColor = Color.Red;
+                lblPasswordError.Text = "Enter this area!";
+                checkPassword = false;
+            }
+            else
+            {
+                lblPasswordError.ForeColor = Color.Green;
+                lblPasswordError.Text = "✅";
+                checkPassword = true;
+            }
+        }
+
+        private void txtConfirmPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtConfirmPassword.Text.Length == 0)
+            {
+                lblConfirmPasswordError.ForeColor = Color.Red;
+                lblConfirmPasswordError.Text = "Enter this area!";
+                checkConfirmPassword = false;
+            }
+            else if (txtConfirmPassword.Text != txtPassword.Text)
+            {
+                lblConfirmPasswordError.ForeColor = Color.Red;
+                lblConfirmPasswordError.Text = "You should enter the same Password";
+                checkConfirmPassword = false;
+            }
+            else
+            {
+                lblConfirmPasswordError.ForeColor = Color.Green;
+                lblConfirmPasswordError.Text = "✅";
+                checkConfirmPassword = true;
+            }
+        }
+
+        private void cbxUsername_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtCMND.Text = dt.Rows[cbxUsername.SelectedIndex]["CMND"].ToString();
+            txtFullName.Text = dt.Rows[cbxUsername.SelectedIndex]["HOTEN"].ToString();
+            txtPhoneNumber.Text = dt.Rows[cbxUsername.SelectedIndex]["SODT"].ToString();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnShowPassword_Click(object sender, EventArgs e)
+        {
+            isShowingPassword = !isShowingPassword;
+            txtPassword.UseSystemPasswordChar = isShowingPassword;
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Program.ValidateKeyPress(e.KeyChar))
+            { e.Handled = true; }
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if(checkLoginName && checkPassword && checkConfirmPassword)
+            {
+                loginName = txtLoginName.Text;
+                password = txtPassword.Text;
+                username = cbxUsername.Text;
+                if (MessageBox.Show("Do you want to create this login account?", "CONFIRM!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    try
+                    {
+                        Program.CreateLoginToSystem(loginName, username, password, Program.mRole);
+                        MessageBox.Show($"This login account has been created!\nLoginName: {loginName}\nUsername: {username}\n", "SUCCESS!", MessageBoxButtons.OK);
+                        this.Refresh();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Can not create login!\n" + ex.Message, "ERROR!", MessageBoxButtons.OK);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your should complete your form to CONFIRM!");
+            }
+        }
+    }
+}
